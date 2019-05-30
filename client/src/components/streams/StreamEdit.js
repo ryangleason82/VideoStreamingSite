03a30b89c7f16635a2e14aec2506bce91594b8d4
Ryan Gleason <ryangleason82@gmail.com>
2019-05-30 06:38:07 -0400

    Passsing state into edit stream, fetching immediately

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,21 @@
-import React from "react";
+import { connect } from "react-redux";
+import React, { Component } from "react";
+import { fetchStream } from "../../actions";
 
-const StreamEdit = () => {
-	return <div>StreamEdit</div>;
+class StreamEdit extends Component {
+	componentDidMount() {
+		this.props.fetchStream(this.props.match.params.id);
+	}
+	render() {
+		console.log(this.props);
+		return <div>StreamEdit</div>;
+	}
+}
+const mapStateToProps = (state, ownProps) => {
+	return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamEdit;
+export default connect(
+	mapStateToProps,
+	{ fetchStream }
+)(StreamEdit);
