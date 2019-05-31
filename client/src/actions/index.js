1f104467cf983e001058100776dc32ae51c934ea
Ryan Gleason <ryangleason82@gmail.com>
2019-05-31 08:29:21 -0400

    Wiring in streams

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -50,4 +50,5 @@ export const editStream = (id, formValues) => async dispatch => {
 export const deleteStream = id => async dispatch => {
 	await streams.delete(`/streams/${id}`);
 	dispatch({ type: DELETE_STREAMS, payload: id });
+	history.push("/");
 };
